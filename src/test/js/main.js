d4a79b13590e78f3b206cf3d8e56bd536c838656
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-06-10 19:22:58 +0100

    main.js port change to match Main's user standard

--- src/test/js/main.js
+++ src/test/js/main.js
@@ -1,4 +1,4 @@
-socket = new WebSocket("ws://localhost:80")
+socket = new WebSocket("ws://localhost:8080")
 meeting = '[{"action": "meeting", "death": "emergency"}]'
 vote_skip = '[{"action": "vote","target": "skip"}]'
 setup = '[{"action": "setup","playerID": "testplayer"}]'
