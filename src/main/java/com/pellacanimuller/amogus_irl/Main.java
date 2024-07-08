8537a49069c375985b9a0f045105d7d230231e1c
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-07-08 22:33:41 +0000

    Read cli for shutdown of server

--- src/main/java/com/pellacanimuller/amogus_irl/Main.java
+++ src/main/java/com/pellacanimuller/amogus_irl/Main.java
@@ -2,20 +2,39 @@ package com.pellacanimuller.amogus_irl;
 
 import com.pellacanimuller.amogus_irl.game.Game;
 import com.pellacanimuller.amogus_irl.net.GameWSServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+    private final static Logger log = LogManager.getLogger(Game.class);
+
+	public static void main(String[] args) throws InterruptedException {
         // STARTUP PROCEDURES:
         // 1. create game , set to lobby
         // 2. Create WS server
         // 3. Wait and react to game_start call
         // 4. Activate listeners, setup roles and tasks
         // 5. game loop
-        Game game = new Game(8);
+        Game game = new Game();
         InetSocketAddress address = new InetSocketAddress("localhost", 8080);
         GameWSServer gameserver = new GameWSServer(address, game);
         gameserver.start();
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String cmd = in.nextLine();
+            cmd = cmd.strip();
+            switch (cmd) {
+                case "exit", "stop" -> {
+                    gameserver.stop();
+                    log.info("Stopping Server");
+                    Thread.sleep(4000);
+                    return;
+                }
+            }
+        }
     }
 }
