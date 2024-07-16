0af4408903ebc1433e1edd5d168a16c6c22d32d1
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-07-16 01:57:33 +0000

    Remove dir test/js

--- src/test/js/main.js
+++
@@ -1,4 +0,0 @@
-socket = new WebSocket("ws://localhost:8080")
-meeting = '[{"action": "meeting", "death": "emergency"}]'
-vote_skip = '[{"action": "vote","target": "skip"}]'
-setup = '[{"action": "setup","playerID": "testplayer"}]'
